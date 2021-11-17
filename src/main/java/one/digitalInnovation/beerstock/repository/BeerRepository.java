eec0e6758da689e08e114e69343e9179d43adf6f
Gabriel Henrique <gabrielhc.l@hotmail.com>
2021-11-17 14:28:02 -0300

    Start and end of the projec

---
+++ src/main/java/one/digitalInnovation/beerstock/repository/BeerRepository.java
@@ -0,0 +1,12 @@
+package one.digitalInnovation.beerstock.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entity.Beer;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByName(String name);
+}
---
+++ src/test/java/one/digitalInnovation/beer/BeerDioApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalInnovation.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
